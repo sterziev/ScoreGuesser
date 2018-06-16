e42ec37b62820ddcce7373250d6591059d80657f
sterziev <stoil.terziev@abv.bg>
2018-06-16 15:05:45 +0300

    storred precedure and points

--- src/main/java/ex/guesser/areas/errorHandling/controller/CustomErrorController.java
+++ src/main/java/ex/guesser/areas/errorHandling/controller/CustomErrorController.java
@@ -1,29 +1,29 @@
 package ex.guesser.areas.errorHandling.controller;//package ex.guesser.areas.errorHandling.controller;
-//
-//import ex.guesser.areas.errorHandling.errors.CommonException;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import javax.servlet.http.HttpServletResponse;
-//
-//@Controller
-//public class CustomErrorController implements ErrorController {
-//    private static final String ERROR_PATH = "/error";
-//
-//    @Override
-//    public String getErrorPath() {
-//        return ERROR_PATH;
-//    }
-//
-//    @RequestMapping(value = ERROR_PATH)
-//    public void error(HttpServletResponse response) {
-//        int statusCode = response.getStatus();
-//        HttpStatus status = HttpStatus.valueOf(statusCode);
-//
-//        String message = String .format("Status code: %s Reason: %s%n%s", status.value(), status.getReasonPhrase(),status.toString());
-//
-//        throw new CommonException(message);
-//    }
-//}
+
+import ex.guesser.areas.errorHandling.errors.CommonException;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    private static final String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @RequestMapping(value = ERROR_PATH)
+    public void error(HttpServletResponse response) {
+        int statusCode = response.getStatus();
+        HttpStatus status = HttpStatus.valueOf(statusCode);
+
+        String message = String .format("Status code: %s Reason: %s%n%s", status.value(), status.getReasonPhrase(),status.toString());
+
+        throw new CommonException(message);
+    }
+}
--- src/main/java/ex/guesser/areas/errorHandling/controller/GlobalExceptionController.java
+++ src/main/java/ex/guesser/areas/errorHandling/controller/GlobalExceptionController.java
@@ -1,34 +1,34 @@
 package ex.guesser.areas.errorHandling.controller;//package ex.guesser.areas.errorHandling.controller;
-//
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//
-//@ControllerAdvice
-//public class GlobalExceptionController {
-//
-//    @ExceptionHandler(RuntimeException.class)
-//    public ModelAndView getException(RuntimeException e) {
-//        String errorMessage = "";
-//
-//        if (e.getMessage()==null){
-//            if (e.getClass().isAnnotationPresent(ResponseStatus.class)) {
-//                errorMessage = e.getClass().getAnnotation(ResponseStatus.class).reason();
-//            } else {
-//                errorMessage = "Error";
-//            }
-//
-//        }
-//        else {
-//            errorMessage = e.getMessage();
-//        }
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("error/error-template");
-//        modelAndView.addObject("error", errorMessage);
-//        return modelAndView;
-//    }
-//}
-//
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ModelAndView getException(RuntimeException e) {
+        String errorMessage = "";
+
+        if (e.getMessage()==null){
+            if (e.getClass().isAnnotationPresent(ResponseStatus.class)) {
+                errorMessage = e.getClass().getAnnotation(ResponseStatus.class).reason();
+            } else {
+                errorMessage = "Error";
+            }
+
+        }
+        else {
+            errorMessage = e.getMessage();
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error/error-template");
+        modelAndView.addObject("error", errorMessage);
+        return modelAndView;
+    }
+}
+
